d69a1c4649a1ef8c45aa76b6fe3768d2cf193b42
mzl <you@example.com>
2016-08-31 10:37:49 +0800

    配置

---
+++ HXXmppTest/HXXmppTest/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  HXXmppTest
+//
+//  Created by winbei on 16/8/31.
+//  Copyright © 2016年 winbei. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ HXXmppTest/HXXmppTest/ViewController.m
@@ -0,0 +1,23 @@
+//
+//  ViewController.m
+//  HXXmppTest
+//
+//  Created by winbei on 16/8/31.
+//  Copyright © 2016年 winbei. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+@end
